feb387105f8760a54824a9d53100081c41ca4518
Devin Lee <jy4ny@virginia.edu>
2015-10-20 13:43:57 -0400

    adding kitchen docker stuff

--- cookbooks/setup_hello_world/recipes/default.rb
+++ cookbooks/setup_hello_world/recipes/default.rb
@@ -3,3 +3,17 @@
 # Recipe:: default
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
+
+package 'httpd'
+
+service 'httpd' do
+  action [:enable, :start]
+end
+
+file '/var/www/html/index.html' do
+  content '<html>
+  <body>
+    <h1>Hello Coveros!!</h1>
+  </body>
+</html>'
+end
