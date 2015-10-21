2e38373be16611550723e0d32f9a74948b6a9521
devin909 <devin.lee@coveros.com>
2015-10-21 16:11:58 -0400

    added tomcat stuff

--- cookbooks/setup_hello_world/recipes/default.rb
+++ cookbooks/setup_hello_world/recipes/default.rb
@@ -4,16 +4,33 @@
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
-package 'httpd'
 
-service 'httpd' do
-  action [:enable, :start]
+
+# install tomcat
+yum_package 'tomcat' do
+	options "-y"
 end
 
-file '/var/www/html/index.html' do
-  content '<html>
-  <body>
-    <h1>Hello Coveros!!</h1>
-  </body>
-</html>'
+# yum install wget
+yum_package 'wget' do
+	options "-y"
+end
+
+remote_file '/home/centos/hello-world-sample-random-gen-1.0.2-rpm.rpm' do
+  source 'http://ec2-54-210-118-101.compute-1.amazonaws.com:8081/nexus/service/local/repositories/releases/content/com/coveros/hello-world-sample-random-gen/1.0.2/hello-world-sample-random-gen-1.0.2-rpm.rpm'
+  owner 'centos'
+  mode '0755'
+  action :create
+end
+
+rpm_package 'hello-world-sample-random-gen-1.0.2-rpm' do
+  action :install
+  source '/home/centos/hello-world-sample-random-gen-1.0.2-rpm.rpm'
+end
+
+bash 'restart_tomcat' do
+  cwd ::File.dirname('/home/centos')
+  code <<-EOH
+    sudo service tomcat restart
+    EOH
 end
