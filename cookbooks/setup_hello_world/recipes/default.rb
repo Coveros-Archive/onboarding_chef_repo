6760e6e21e8aa8a68c9a91f357602e64ad4e3c0e
devin909 <devin.lee@coveros.com>
2015-10-21 16:28:57 -0400

    added tomcat stuff

--- cookbooks/setup_hello_world/recipes/default.rb
+++ cookbooks/setup_hello_world/recipes/default.rb
@@ -16,7 +16,7 @@ yum_package 'wget' do
 	options "-y"
 end
 
-remote_file '/home/centos/hello-world-sample-random-gen-1.0.2-rpm.rpm' do
+remote_file '/tmp/hello-world-sample-random-gen-1.0.2-rpm.rpm' do
   source 'http://ec2-54-210-118-101.compute-1.amazonaws.com:8081/nexus/service/local/repositories/releases/content/com/coveros/hello-world-sample-random-gen/1.0.2/hello-world-sample-random-gen-1.0.2-rpm.rpm'
   mode '0755'
   action :create
